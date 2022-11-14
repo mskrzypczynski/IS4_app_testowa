fc461b7124779c08b4cb2726eb394c9f3a6cfc03
mskrzypczynski <mikolaj.skrzypczynski@gmail.com>
2022-11-14 12:19:16 +0100

    aaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,51 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    private JLabel lblManufacturer;
+    private JTextField txtManufacturer;
+    private JButton btnStartTest;
+
+
+    Main(){
+        super();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("IS4_Aplikacja_testowa_Skrzypczynski_Mikolaj");
+        setSize(800,600);
+        setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+        initGUI();
+        setVisible(true);
+
+
+
     }
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+
+    void initGUI()
+    {
+        lblManufacturer = new JLabel("test");
+        add(lblManufacturer);
+        txtManufacturer = new JTextField();
+        add(txtManufacturer);
+        btnStartTest    = new JButton("test");
+        add(btnStartTest);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
